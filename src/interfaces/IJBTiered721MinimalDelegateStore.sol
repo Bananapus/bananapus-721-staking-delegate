088bf9877602425a9054753984cff3dadf62860d
xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2023-05-18 20:43:46 +0200

    fix: incorrect interface file names

--- src/interfaces/IJBTiered721MinimalDelegateStore.sol
+++ src/interfaces/IJBTiered721MinimalDelegateStore.sol
@@ -1,10 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+import "@jbx-protocol/juice-721-delegate/contracts/structs/JB721Tier.sol";
+import "@jbx-protocol/juice-721-delegate/contracts/structs/JBTiered721Flags.sol";
+
 /**
- * @notice Interface for 721Delegate that has all the required methods for UI support.
+ * @notice Interface for 721DelegateStore that has all the required methods for UI support.
  */
 interface IJBTiered721MinimalDelegateStore {
-    function contractURI() external view returns (string memory);
-    function store() external view returns (address);
+    function tierOf(
+        address _nft,
+        uint256 _id,
+        bool _includeResolvedUri
+    ) external view returns (JB721Tier memory tier);
+
+    function flagsOf(
+        address _nft
+    ) external view returns (JBTiered721Flags memory);
+
+    function redemptionWeightOf(
+        address _nft,
+        uint256[] memory _tokenIds
+    ) external view returns (uint256 weight);
+
+    function totalRedemptionWeight(
+        address _nft
+    ) external view returns (uint256 weight);
 }
+
